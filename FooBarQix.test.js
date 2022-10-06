29b49ffeace445f09d213e0c16013d2b44a444ed
crnndavid <95691870+crnndavid@users.noreply.github.com>
2022-10-06 15:54:06 +0200

    add replaceValue function and Test;

--- FooBarQix.test.js
+++ FooBarQix.test.js
@@ -1,6 +1,6 @@
 const { describe, test, expect } = require("@jest/globals");
 const { default: expect_2 } = require("expect");
-const { translateNumByValues } = require("./FooBarQix");
+const { translateNumByValues, replaceValue } = require("./FooBarQix");
 
 describe("String should be evaluated", () => {
   test("should be divisible by 3, 5 and 7", () => {
@@ -14,5 +14,16 @@ describe("String should be evaluated", () => {
   test("should be divisible only by 3", () => {
     const value = "3";
     expect(translateNumByValues(value)).toBe("Foo");
+  });
+});
+
+describe("String containing 3 , 5 , 7 or 0", () => {
+  test("should contain 0 and be replace by *", () => {
+    const value = ["1", "0", "1"];
+    expect(replaceValue(value)).toStrictEqual(["1", "*", "1"]);
+  });
+  test("should return arr with Foo, Bar, Qix and *", () => {
+    const value = ["3", "5", "7", "0"];
+    expect(replaceValue(value)).toStrictEqual(["Foo", "Bar", "Qix", "*"]);
   });
 });
